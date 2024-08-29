17d58f0f72311ee0bb026079229d37ff65f3835e
ashik-ad <dev.ashikdhimal@gmail.com>
2024-08-29 17:46:08 +0545

    TODO: make workable link

--- frontend/src/components/project-structure/index.tsx
+++ frontend/src/components/project-structure/index.tsx
@@ -5,6 +5,7 @@ import { PiMarkdownLogoFill } from 'react-icons/pi'
 import { IoCubeSharp } from 'react-icons/io5'
 
 import styles from './styles.module.css'
+import { useState } from 'react'
 
 type Project = {
     id: string
@@ -19,49 +20,60 @@ type ProjectStructure = Project & {
 }
 
 export default function ProjectStructure(props: ProjectStructure) {
+    const [isExtended, setIsExtended] = useState(false)
+
     let nodeClassName = `${styles.struct__node} text-small`
     return (
-        <Stack gap="0" className={styles.project_structure}>
+        <Stack
+            gap="0"
+            className={`${styles.project_structure} ${isExtended ? styles['project_structure-extend'] : styles['project_structure-unextend']}`}
+        >
             <Link
-                to={`projects/${props.id}`}
+                to={`#`}
                 className={`text-medium ${styles.struct__root} ${!props.isChild ? styles.project__root : styles.sub__project__root}`}
+                onClick={() => setIsExtended((prev) => !prev)}
             >
-                {!props.isChild && (
-                    <i className={styles.root__icon}>
-                        <IoCubeSharp />
-                    </i>
-                )}
+                <i className={styles.root__icon}>
+                    <IoCubeSharp />
+                </i>
                 <span>{props.name}</span>
             </Link>
-            {props.childProjects?.map((project) => (
-                <Stack key={project.id} className={styles.project__struct__sub}>
-                    <ProjectStructure {...project} isChild />
+            <Stack className={styles.project__tree} gap="0px">
+                {props.childProjects?.map((project) => (
+                    <Stack
+                        key={project.id}
+                        className={styles.project__struct__sub}
+                    >
+                        <ProjectStructure {...project} isChild />
+                    </Stack>
+                ))}
+                <Stack
+                    gap="0"
+                    className={
+                        !props.isChild
+                            ? styles.root__files
+                            : styles.child__files
+                    }
+                >
+                    <Link to={`./env`} className={nodeClassName}>
+                        <i className={styles.file__icon}>
+                            <HiMiniAdjustmentsVertical />
+                        </i>
+                        <span>{props.files[0].name}</span>
+                    </Link>
+                    <Link to={`/packages`} className={nodeClassName}>
+                        <i className={styles.file__icon}>
+                            <NodeIcon />
+                        </i>
+                        <h5>{props.files[1].name}</h5>
+                    </Link>
+                    <Link to={`/readme`} className={styles.struct__node}>
+                        <i className={styles.file__icon}>
+                            <PiMarkdownLogoFill />
+                        </i>
+                        <h5>{props.files[2].name}</h5>
+                    </Link>
                 </Stack>
-            ))}
-            <Stack
-                gap="0"
-                className={
-                    !props.isChild ? styles.root__files : styles.child__files
-                }
-            >
-                <Link to={`./env`} className={nodeClassName}>
-                    <i className={styles.file__icon}>
-                        <HiMiniAdjustmentsVertical />
-                    </i>
-                    <span>{props.files[0].name}</span>
-                </Link>
-                <Link to={`/packages`} className={nodeClassName}>
-                    <i className={styles.file__icon}>
-                        <NodeIcon />
-                    </i>
-                    <h5>{props.files[1].name}</h5>
-                </Link>
-                <Link to={`/readme`} className={styles.struct__node}>
-                    <i className={styles.file__icon}>
-                        <PiMarkdownLogoFill />
-                    </i>
-                    <h5>{props.files[2].name}</h5>
-                </Link>
             </Stack>
         </Stack>
     )
--- frontend/src/stories/project-structure/ProjectStructure.stories.ts
+++ frontend/src/stories/project-structure/ProjectStructure.stories.ts
@@ -176,3 +176,107 @@ export const NestedProjectLevel3: Story = {
         ],
     },
 }
+
+export const NestedProjectLevel4: Story = {
+    args: {
+        id: '123l2',
+        path: '/home/lazyduck/dev/node-nanny',
+        name: 'node-nanny',
+        files: [
+            {
+                type: 'emv',
+                name: '.env',
+            },
+            {
+                type: 'json',
+                name: 'package.json',
+            },
+            {
+                type: 'md',
+                name: 'README.md',
+            },
+        ],
+        childProjects: [
+            {
+                id: '224323',
+                path: '/home/lazyduck/dev/node-nanny/frontend',
+                name: 'frontend',
+                childProjects: [
+                    {
+                        id: 'a24k2das',
+                        path: '/home/lazyduck/dev/node-nanny/backend',
+                        name: 'react',
+                        files: [
+                            {
+                                type: 'emv',
+                                name: '.env',
+                            },
+                            {
+                                type: 'json',
+                                name: 'package.json',
+                            },
+                            {
+                                type: 'md',
+                                name: 'README.md',
+                            },
+                        ],
+                        childProjects: [
+                            {
+                                id: 'a24k2da33',
+                                path: '/home/lazyduck/dev/node-nanny/backend',
+                                name: 'solid',
+                                files: [
+                                    {
+                                        type: 'emv',
+                                        name: '.env',
+                                    },
+                                    {
+                                        type: 'json',
+                                        name: 'package.json',
+                                    },
+                                    {
+                                        type: 'md',
+                                        name: 'README.md',
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+                files: [
+                    {
+                        type: 'emv',
+                        name: '.env',
+                    },
+                    {
+                        type: 'json',
+                        name: 'package.json',
+                    },
+                    {
+                        type: 'md',
+                        name: 'README.md',
+                    },
+                ],
+            },
+            {
+                id: 'a24k2',
+                path: '/home/lazyduck/dev/node-nanny/backend',
+                name: 'backend',
+                files: [
+                    {
+                        type: 'emv',
+                        name: '.env',
+                    },
+                    {
+                        type: 'json',
+                        name: 'package.json',
+                    },
+                    {
+                        type: 'md',
+                        name: 'README.md',
+                    },
+                ],
+            },
+        ],
+    },
+}
