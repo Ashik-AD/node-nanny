80d07c2961dac3ac5bba837c991520e4f55dafd0
ashik-ad <dev.ashikdhimal@gmail.com>
2024-09-17 15:15:15 +0545

    change size

--- frontend/src/components/package-actions/index.tsx
+++ frontend/src/components/package-actions/index.tsx
@@ -14,29 +14,31 @@ type Props = {
 export default function PackageActions(props: Props) {
     const { id, onHandleCheckUpdate, onHandleUpdate, onHandleRemove } = props
     return (
-        <Stack gap='16px' dir="column">
+        <Stack gap="16px" dir="column">
             <Button
-                startContent={<MdUpdate size={24} />}
+                startContent={<MdUpdate size={20} />}
                 radius="full"
                 color="primary"
                 title="Update package"
-                size='sm'
+                size="sm"
                 onClick={() => onHandleCheckUpdate(id)}
                 isIconOnly
             />
             <Button
-                startContent={<MdOutlineDownloading size={24} />}
+                startContent={<MdOutlineDownloading size={20} />}
                 radius="full"
                 color="secondary"
                 title="Install package"
+                size="sm"
                 onClick={() => onHandleUpdate(id)}
                 isIconOnly
             />
             <Button
-                startContent={<TbTrash size={24} />}
+                startContent={<TbTrash size={20} />}
                 radius="full"
                 color="warning"
                 title="Remove package"
+                size="sm"
                 onClick={() => onHandleRemove(id)}
                 isIconOnly
             />
